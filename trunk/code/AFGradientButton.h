e15ab40d7f46d29a1e2656ec331d2bdb88b57718
keith_duncan@mac.com <keith_duncan@mac.com@3c3a8548-80c5-11dd-b774-4f160e1b508b>
2009-05-16 20:48:00 +0000

    Code fiddling. Removed instance variable access. Fixed copyright headers. Renamed private geometry functions. Updated observation context to modern implementation.

--- trunk/code/AFGradientButton.h
+++ trunk/code/AFGradientButton.h
@@ -1,6 +1,6 @@
 //
-//  KDGradientButton.h
-//  KDSliderControl
+//  AFGradientButton.h
+//  AFGradientButton
 //
 //  Created by Keith Duncan on 06/07/2008.
 //  Copyright 2008 thirty-three software. All rights reserved.
@@ -8,6 +8,10 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class AFGradientCell;
+
 @interface AFGradientButton : NSButton
+
+@property (retain) AFGradientCell *cell;
 
 @end
