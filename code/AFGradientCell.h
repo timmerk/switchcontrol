014da42b82440619fe379b2b72904e9bb436cadf
keith_duncan <keith_duncan@mac.com>
2009-05-16 21:18:35 +0000

    Documentation++

--- code/AFGradientCell.h
+++ code/AFGradientCell.h
@@ -8,12 +8,21 @@
 
 #import <Cocoa/Cocoa.h>
 
+/*
+	@brief	A very simple cell subclass which draws a gray gradient (the one seen in <tt>AFGradientButton</tt> and <tt>AFSwitchControl</tt>) in a provided rect with rounded corners.
+ */
 @interface AFGradientCell : NSButtonCell {
 	CGFloat _cornerRadius;
 }
 
+/*
+	@brief	This property must be set before drawing with either <tt>-drawWithFrame:inView:</tt> or <tt>-drawBezelWithFrame:inView:</tt>.
+ */
 @property (assign) CGFloat cornerRadius;
 
+/*
+	@brief	The gradient is drawn in this method, allowing its user elsewhere without the text drawn too.
+ */
 - (void)drawBezelWithFrame:(NSRect)frame inView:(NSView *)view;
 
 @end
